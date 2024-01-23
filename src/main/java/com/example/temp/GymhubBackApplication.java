58bcacc3fa1b7576c82cabb76fd9eb56b82d0744
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-23 14:27:03 +0900

    first commit

---
+++ src/main/java/com/example/temp/GymhubBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.temp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymhubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymhubBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/temp/GymhubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymhubBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
