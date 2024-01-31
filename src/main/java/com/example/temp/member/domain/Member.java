3d253894f5b85d4ebe4875396f2c0cf9a6046ee1
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-31 19:14:46 +0900

    [#6] feat: Member 엔티티의 email 컬럼에서 unique 제약 조건 삭제

--- src/main/java/com/example/temp/member/domain/Member.java
+++ src/main/java/com/example/temp/member/domain/Member.java
@@ -21,7 +21,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
