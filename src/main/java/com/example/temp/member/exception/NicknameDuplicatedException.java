9287cab7bd9576d492a944814ec076ead00a2c60
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-31 20:37:12 +0900

    [#6] refactor: NicknameDuplicatedException 에러 메세지 상수화

--- src/main/java/com/example/temp/member/exception/NicknameDuplicatedException.java
+++ src/main/java/com/example/temp/member/exception/NicknameDuplicatedException.java
@@ -2,7 +2,9 @@ package com.example.temp.member.exception;
 
 public class NicknameDuplicatedException extends RuntimeException {
 
+    public static final String NICKNAME_DUPLICATED_MSG = "서버가 중복된 닉네임으로 회원 가입을 시도하고 있습니다.";
+
     public NicknameDuplicatedException(Throwable cause) {
-        super("서버가 중복된 닉네임으로 회원 가입을 시도하고 있습니다.", cause);
+        super(NICKNAME_DUPLICATED_MSG, cause);
     }
 }
