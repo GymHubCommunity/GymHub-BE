f6d416762bf950884979815c3a72d343f857691d
kimtaehoonDev <thkimm.dev@gmail.com>
2024-01-31 16:53:55 +0900

    [#6] feat: MemberService register 기능 구현

--- src/main/java/com/example/temp/member/application/MemberService.java
+++ src/main/java/com/example/temp/member/application/MemberService.java
@@ -1,9 +1,11 @@
 package com.example.temp.member.application;
 
 import com.example.temp.member.domain.Member;
+import com.example.temp.member.domain.MemberRepository;
 import com.example.temp.member.infrastructure.nickname.NicknameGenerator;
 import com.example.temp.oauth.OAuthResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +14,28 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberRepository memberRepository;
     private final NicknameGenerator nicknameGenerator;
 
+    /**
+     * OAuthResponse과 nicknameGenerator에서 생성한 닉네임을 사용해 Member를 저장합니다.
+     *
+     * @param oAuthResponse
+     * @return 저장된 Member 객체를 반환합니다.
+     * @throws DataIntegrityViolationException 중복된 닉네임으로 Member를 저장하려 할 때 발생합니다.
+     * 이 예외는 MemberServiceFacade 계층에서 처리합니다.
+     */
     @Transactional
     public Member register(OAuthResponse oAuthResponse) {
         String nickname = nicknameGenerator.generate();
-        return null;
+
+        Member member = Member.builder()
+            .email(oAuthResponse.email())
+            .profileUrl(oAuthResponse.profileUrl())
+            .nickname(nickname)
+            .build();
+        memberRepository.save(member);
+        return member;
     }
 
 }
