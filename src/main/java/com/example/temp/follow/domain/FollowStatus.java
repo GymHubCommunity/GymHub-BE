657507aa4149bdf34ec6dd2d7004042be22b94e4
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-03 14:48:24 +0900

    [#13] feat: FollowStatus 생성

--- src/main/java/com/example/temp/follow/domain/FollowStatus.java
+++ src/main/java/com/example/temp/follow/domain/FollowStatus.java
@@ -1,5 +1,14 @@
 package com.example.temp.follow.domain;
 
 public enum FollowStatus {
+    SUCCESS("성공"),
+    PENDING("보류"),
+    REJECTED("거절"),
+    CANCELED("취소");
 
+    private final String text;
+
+    FollowStatus(String text) {
+        this.text = text;
+    }
 }
