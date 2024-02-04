98653eb3c671881ac0d4478c8ac35c4ad0efa79e
JWbase <jungwoo1002@gmail.com>
2024-02-04 23:34:01 +0900

    [GymHubCommunity#23] feat: KakaoOAuthClient 구현

--- src/main/java/com/example/temp/oauth/impl/kakao/KakaoOAuthClient.java
+++ src/main/java/com/example/temp/oauth/impl/kakao/KakaoOAuthClient.java
@@ -1,6 +1,19 @@
 package com.example.temp.oauth.impl.kakao;
 
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
 public interface KakaoOAuthClient {
 
+    @PostExchange(url = "https://kauth.kakao.com/oauth/token", contentType = APPLICATION_FORM_URLENCODED_VALUE)
+    KakaoToken fetchToken(@RequestBody MultiValueMap<String, String> params);
 
+    @GetExchange(url = "https://kapi.kakao.com/v2/user/me")
+    KakaoUserInfo fetchUserInfo(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String authorizationValue);
 }
