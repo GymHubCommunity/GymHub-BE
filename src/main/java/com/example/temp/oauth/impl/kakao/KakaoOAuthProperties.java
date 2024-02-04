34f0f052d6a77186f20406f5a09a2067c624c21d
JWbase <jungwoo1002@gmail.com>
2024-02-04 18:20:02 +0900

    [GymHubCommunity#23] feat: KakaoOAuthProperties 추가

---
+++ src/main/java/com/example/temp/oauth/impl/kakao/KakaoOAuthProperties.java
@@ -0,0 +1,45 @@
+package com.example.temp.oauth.impl.kakao;
+
+import java.util.Arrays;
+import java.util.Objects;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth.kakao")
+public record KakaoOAuthProperties(
+    String clientId,
+    String clientSecret,
+    String redirectUri,
+    String[] scope
+) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        KakaoOAuthProperties that = (KakaoOAuthProperties) o;
+        return Objects.equals(clientId, that.clientId) && Objects.equals(clientSecret,
+            that.clientSecret) && Objects.equals(redirectUri, that.redirectUri) && Arrays.equals(scope,
+            that.scope);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(clientId, clientSecret, redirectUri);
+        result = 31 * result + Arrays.hashCode(scope);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "KakaoOAuthProperties{" +
+            "clientId='" + clientId + '\'' +
+            ", clientSecret='" + clientSecret + '\'' +
+            ", redirectUri='" + redirectUri + '\'' +
+            ", scope=" + Arrays.toString(scope) +
+            '}';
+    }
+}
