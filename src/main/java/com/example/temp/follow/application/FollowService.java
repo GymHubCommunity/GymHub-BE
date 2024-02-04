9aa050620eda68e716660219ff397a538da56078
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-04 11:58:44 +0900

    [#13] refactor: 가독성 향상

--- src/main/java/com/example/temp/follow/application/FollowService.java
+++ src/main/java/com/example/temp/follow/application/FollowService.java
@@ -25,15 +25,19 @@ public class FollowService {
         Member target = memberRepository.findById(targetId)
             .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 사용자"));
         if (!target.isPublicAccount()) {
-            validateAuthorization(targetId, executorId);
+            validateViewAuthorization(targetId, executorId);
         }
         return followRepository.findAllByFromIdAndStatus(targetId, FollowStatus.SUCCESS).stream()
             .map(follow -> FollowInfo.of(follow.getFrom(), follow.getId()))
             .toList();
     }
 
-    private void validateAuthorization(long targetId, long executorId) {
-        if (targetId == executorId) {
+    public List<FollowInfo> getFollowers(long executorId, long targetId) {
+        return null;
+    }
+
+    private void validateViewAuthorization(long targetId, long executorId) {
+        if (isMyAccount(targetId, executorId)) {
             return;
         }
         if (!isTargetsFollower(targetId, executorId)) {
@@ -41,36 +45,45 @@ public class FollowService {
         }
     }
 
+    private boolean isMyAccount(long targetId, long executorId) {
+        return targetId == executorId;
+    }
+
     private boolean isTargetsFollower(long targetId, long executorId) {
         return followRepository.checkExecutorFollowsTarget(executorId, targetId);
     }
 
-    public List<FollowInfo> getFollowers(long executorId, long targetId) {
-        return null;
-    }
-
     @Transactional
-    public FollowResponse follow(long fromId, Long toId) {
-        if (fromId == toId) {
+    public FollowResponse follow(long executorId, Long targetId) {
+        if (isMyAccount(executorId, targetId)) {
             throw new IllegalArgumentException("자기 자신을 팔로우할 수 없습니다.");
         }
-        Member fromMember = memberRepository.findById(fromId)
+        Member fromMember = memberRepository.findById(executorId)
             .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 사용자"));
-        Member target = memberRepository.findById(toId)
+        Member target = memberRepository.findById(targetId)
             .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 사용자"));
 
-        Follow savedFollow = followRepository.findByFromIdAndToId(fromId, toId)
+        Follow savedFollow = followRepository.findByFromIdAndToId(executorId, targetId)
             .map(follow -> follow.reactive(target.getStatusBasedOnStrategy()))
             .orElseGet(() -> saveFollow(fromMember, target));
         return FollowResponse.from(savedFollow);
     }
 
+    private Follow saveFollow(Member fromMember, Member target) {
+        Follow follow = Follow.builder()
+            .from(fromMember)
+            .to(target)
+            .status(target.getStatusBasedOnStrategy())
+            .build();
+        return followRepository.save(follow);
+    }
+
     @Transactional
-    public void unfollow(long fromId, Long toId) {
-        if (!memberRepository.existsById(toId)) {
+    public void unfollow(long executorId, Long targetId) {
+        if (!memberRepository.existsById(targetId)) {
             throw new IllegalArgumentException("찾을 수 없는 사용자");
         }
-        Follow follow = followRepository.findByFromIdAndToId(fromId, toId)
+        Follow follow = followRepository.findByFromIdAndToId(executorId, targetId)
             .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 관계"));
         follow.unfollow();
     }
@@ -91,19 +104,10 @@ public class FollowService {
         Follow follow = followRepository.findById(followId)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Follow"));
         Member target = follow.getTo();
-        if (target.getId() != executorId) {
+        if (!isMyAccount(target.getId(), executorId)) {
             throw new IllegalArgumentException("권한없음");
         }
         follow.reject();
-    }
-
-    private Follow saveFollow(Member fromMember, Member target) {
-        Follow follow = Follow.builder()
-            .from(fromMember)
-            .to(target)
-            .status(target.getStatusBasedOnStrategy())
-            .build();
-        return followRepository.save(follow);
     }
 
 }
