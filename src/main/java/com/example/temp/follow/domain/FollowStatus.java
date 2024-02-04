9c086c48c1db3a593c2fd5b9a39c94ff50e1cc16
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-05 01:47:48 +0900

    [#13] refactor: FollowStatus의 text 필드 구체적으로 작성

--- src/main/java/com/example/temp/follow/domain/FollowStatus.java
+++ src/main/java/com/example/temp/follow/domain/FollowStatus.java
@@ -3,10 +3,10 @@ package com.example.temp.follow.domain;
 import java.util.List;
 
 public enum FollowStatus {
-    SUCCESS("성공"),
-    PENDING("보류"),
-    REJECTED("거절"),
-    CANCELED("취소");
+    SUCCESS("현재 팔로우중인 상태"),
+    PENDING("팔로우 요청이 승인되지 않은 상태"),
+    REJECTED("팔로우 요청을 거절한 상태"),
+    CANCELED("기존 팔로우를 취소한 상태");
 
     private final String text;
 
