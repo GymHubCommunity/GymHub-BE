b55f14220d03dad04280f52cc2eafc6f5984508a
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-05 15:52:27 +0900

    [#9] feat: Properties에 불필요한 메서드 삭제

--- src/main/java/com/example/temp/common/properties/CorsProperties.java
+++ src/main/java/com/example/temp/common/properties/CorsProperties.java
@@ -1,8 +1,8 @@
 package com.example.temp.common.properties;
 
-import java.util.Arrays;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@SuppressWarnings("java:S6218")
 @ConfigurationProperties(prefix = "cors")
 public record CorsProperties(
     String[] allowedOrigins,
@@ -11,33 +11,4 @@ public record CorsProperties(
     String[] exposedHeaders
 ) {
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        CorsProperties that = (CorsProperties) o;
-        return Arrays.equals(allowedOrigins, that.allowedOrigins) && Arrays.equals(allowedMethods,
-            that.allowedMethods) && Arrays.equals(allowedHeaders, that.allowedHeaders);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Arrays.hashCode(allowedOrigins);
-        result = 31 * result + Arrays.hashCode(allowedMethods);
-        result = 31 * result + Arrays.hashCode(allowedHeaders);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "CorsProperties{" +
-            "allowedOrigins=" + Arrays.toString(allowedOrigins) +
-            ", allowedMethods=" + Arrays.toString(allowedMethods) +
-            ", allowedHeaders=" + Arrays.toString(allowedHeaders) +
-            '}';
-    }
 }
