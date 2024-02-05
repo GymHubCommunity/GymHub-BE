df537f724db2455542fa2bad49ab028d312d0f23
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-05 17:18:48 +0900

    [#4] feat: POST /auth/logout 연동

--- src/main/java/com/example/temp/auth/presentation/AuthController.java
+++ src/main/java/com/example/temp/auth/presentation/AuthController.java
@@ -36,34 +36,44 @@ public class AuthController {
         LoginMemberResponse memberResponse = oAuthService.login(provider, request.authCode());
         TokenInfo tokenInfo = tokenManager.issue(memberResponse.id());
 
-        ResponseCookie refreshCookie = createRefreshCookie(tokenInfo.refreshToken());
-        response.addHeader(SET_COOKIE, refreshCookie.toString());
+        createRefreshCookie(tokenInfo.refreshToken(), response);
         return ResponseEntity.ok(LoginResponse.of(tokenInfo, memberResponse));
-    }
-
-    private ResponseCookie createRefreshCookie(String value) {
-        return ResponseCookie.from(REFRESH, value)
-            .path("/")
-            .httpOnly(true)
-            .secure(refreshCookieProperties.secure())
-            .maxAge(refreshCookieProperties.maxAge())
-            .sameSite(refreshCookieProperties.sameSite())
-            .build();
     }
 
     @PostMapping("/auth/refresh")
     public ResponseEntity<TokenInfo> reissueToken(@CookieValue(REFRESH) String refreshToken,
         HttpServletResponse response) {
         TokenInfo tokenInfo = tokenManager.reIssue(refreshToken);
-        ResponseCookie refreshCookie = createRefreshCookie(tokenInfo.refreshToken());
-        response.addHeader(SET_COOKIE, refreshCookie.toString());
+        createRefreshCookie(tokenInfo.refreshToken(), response);
         return ResponseEntity.ok(tokenInfo);
     }
 
     @PostMapping("/auth/logout")
-    public ResponseEntity<Void> logout() {
-        log.info("로그아웃 성공");
+    public ResponseEntity<Void> logout(HttpServletResponse response) {
+        removeRefreshCookie(response);
         return ResponseEntity.noContent().build();
+    }
+
+    private void createRefreshCookie(String value, HttpServletResponse response) {
+        ResponseCookie refreshCookie = ResponseCookie.from(REFRESH, value)
+            .path("/")
+            .httpOnly(true)
+            .secure(refreshCookieProperties.secure())
+            .maxAge(refreshCookieProperties.maxAge())
+            .sameSite(refreshCookieProperties.sameSite())
+            .build();
+        response.addHeader(SET_COOKIE, refreshCookie.toString());
+    }
+
+    private void removeRefreshCookie(HttpServletResponse response) {
+        ResponseCookie removedCookie = ResponseCookie.from(REFRESH, "")
+            .path("/")
+            .httpOnly(true)
+            .secure(refreshCookieProperties.secure())
+            .maxAge(0)
+            .sameSite(refreshCookieProperties.sameSite())
+            .build();
+        response.addHeader(SET_COOKIE, removedCookie.toString());
     }
 
 }
