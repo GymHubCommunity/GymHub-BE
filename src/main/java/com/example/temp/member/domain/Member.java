dd3335b532edba2bef53e920c75bd54c30ff88ec
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-08 02:24:05 +0900

    [#36] docs: Member 엔티티 JavaDoc 작성

--- src/main/java/com/example/temp/member/domain/Member.java
+++ src/main/java/com/example/temp/member/domain/Member.java
@@ -31,7 +31,7 @@ public class Member {
     private Long id;
 
     /**
-     * 사용자에게 profileUrl과 nickname을 인증받았다면 true, 그렇지 않다면 false를 갖습니다.
+     * 회원가입 처리가 끝난 이후 true 값을 갖습니다.
      */
     private boolean registered;
 
@@ -46,27 +46,31 @@ public class Member {
     @Column(nullable = false, unique = true)
     private Nickname nickname;
 
+    private boolean publicAccount;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private FollowStrategy followStrategy;
 
-    private boolean publicAccount;
-
     @Builder
-    private Member(Email email, boolean registered, String profileUrl, Nickname nickname,
-        FollowStrategy followStrategy, boolean publicAccount) {
-        this.email = email;
+    private Member(boolean registered, Email email, String profileUrl, Nickname nickname,
+        boolean publicAccount, FollowStrategy followStrategy) {
         this.registered = registered;
+        this.email = email;
         this.profileUrl = profileUrl;
         this.nickname = nickname;
-        this.followStrategy = followStrategy;
         this.publicAccount = publicAccount;
+        this.followStrategy = followStrategy;
     }
 
-    public FollowStatus getStatusBasedOnStrategy() {
-        return followStrategy.getFollowStatus();
-    }
-
+    /**
+     * 가입이 완료되지 않은 회원 엔티티를 생성합니다. 해당 회원은 현재 가입되지 않은 상태이고, 비공개 계정이며, LAZY한 팔로우 전략을 갖습니다.
+     *
+     * @param email
+     * @param profileUrl
+     * @param nickname
+     * @return 가입이 완료되지 않은 회원 엔티티를 반환합니다.
+     */
     public static Member createInitStatus(Email email, String profileUrl, Nickname nickname) {
         return Member.builder()
             .registered(false)
@@ -78,6 +82,13 @@ public class Member {
             .build();
     }
 
+    /**
+     * nickname과 profileUrl을 입력받아 회원가입 처리를 완료합니다.
+     *
+     * @param nickname
+     * @param profileUrl
+     * @throws ApiException MEMBER_ALREADY_REGISTER: 이미 가입이 완료된 회원이 해당 메서드를 호출했을 때 발생합니다.
+     */
     public void init(Nickname nickname, String profileUrl) {
         if (registered) {
             throw new ApiException(ErrorCode.MEMBER_ALREADY_REGISTER);
@@ -85,6 +96,14 @@ public class Member {
         this.registered = true;
         this.nickname = nickname;
         this.profileUrl = profileUrl;
+    }
+
+    /**
+     * 해당 회원을 팔로우했을 때, 생성될 팔로우 엔티티의 상태를 반환합니다. FollowStrategy의 text 컬럼을 통해 자세한 전략을 확인할 수 있습니다.
+     * @return 팔로우 엔티티의 상태값을 반환합니다. (ex. APPROVED, PENDING)
+     */
+    public FollowStatus getStatusBasedOnStrategy() {
+        return followStrategy.getFollowStatus();
     }
 
     public String getNicknameStr() {
