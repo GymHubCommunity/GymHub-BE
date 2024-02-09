c7f160a15e7c5c256e5aca1170e35cf2562cc9ba
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-09 12:04:15 +0900

    [#49] refactor: Soft Delete를 적용으로 인해 변경되는 MemberRepository 쿼리 수정

--- src/main/java/com/example/temp/member/domain/MemberRepository.java
+++ src/main/java/com/example/temp/member/domain/MemberRepository.java
@@ -1,12 +1,26 @@
 package com.example.temp.member.domain;
 
 import com.example.temp.member.domain.nickname.Nickname;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END FROM Member m"
+        + " WHERE m.nickname = :nickname"
+        + " AND m.deleted = false")
     boolean existsByNickname(Nickname nickname);
+
+    @Query("SELECT m FROM Member m WHERE m.id = :memberId AND m.deleted = false")
+    Optional<Member> findById(@Param(value = "memberId") long memberId);
+
+    @Query("SELECT m FROM Member m WHERE m.id = :memberId"
+        + " AND m.registered = true"
+        + " AND m.deleted = false")
+    Optional<Member> findRegisteredMemberById(@Param(value = "memberId") long memberId);
 
 }
