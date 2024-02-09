d697a2439a279c916d5048cbee7dc9ec71aea522
JWbase <jungwoo1002@gmail.com>
2024-02-10 00:58:49 +0900

    [GymHubCommunity#39] refactor: Content 엔티티 equals, hasCode 메소드 명시 제거

--- src/main/java/com/example/temp/post/domain/Content.java
+++ src/main/java/com/example/temp/post/domain/Content.java
@@ -5,7 +5,6 @@ import com.example.temp.common.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
-import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,21 +31,5 @@ public class Content {
         if (value.length() > MAX_CONTENT_LENGTH) {
             throw new ApiException(ErrorCode.CONTENT_TOO_LONG);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Content content)) {
-            return false;
-        }
-        return Objects.equals(value, content.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }
