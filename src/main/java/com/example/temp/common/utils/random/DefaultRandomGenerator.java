7447ce1b573b040b4ca89a1eea8200f2b2ed280f
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-11 13:00:06 +0900

    [#56] refactor: DefaultRandomGenerator 중복 메서드 추출

--- src/main/java/com/example/temp/common/utils/random/DefaultRandomGenerator.java
+++ src/main/java/com/example/temp/common/utils/random/DefaultRandomGenerator.java
@@ -3,41 +3,57 @@ package com.example.temp.common.utils.random;
 import java.util.Random;
 import org.springframework.stereotype.Component;
 
+/**
+ * 랜덤한 난수를 생성합니다. 생성되는 난수는 [알파벳 대소문자, 숫자] 로 구성되어 있습니다. 기본으로 생성되는 난수의 길이는 20입니다.
+ *
+ * @warning 보안에 관련한 용도로는 사용해서는 안됩니다. 해당 생성기로 만들어지는 난수는 예측이 가능합니다.
+ */
 @Component
+@SuppressWarnings("java:S2245")
 public class DefaultRandomGenerator implements RandomGenerator {
 
-    public static final int DEFAULT_SIZE = 10;
+    public static final int DEFAULT_SIZE = 20;
     private static final String DEFAULT_CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
-    private final Random random = new Random(12312);
+    private final Random random = new Random("GymHub".hashCode());
 
+    /**
+     * @param length
+     * @return 길이가 length인 난수
+     */
     @Override
-    public String generate(int size) {
-        StringBuilder randomStr = new StringBuilder(size);
-        for (int i = 0; i < size; i++) {
-            int number = this.random.nextInt(DEFAULT_CHARSET.length());
-            char ch = DEFAULT_CHARSET.charAt(number);
-            randomStr.append(ch);
-        }
-        return randomStr.toString();
+    public String generate(int length) {
+        return generateHelper(length, this.random);
     }
 
+    /**
+     * 길이가 DEFAULT인 난수를 반환합니다.
+     */
     @Override
     public String generate() {
         return generate(DEFAULT_SIZE);
     }
 
+    /**
+     * 길이가 length인 고정된 난수를 반환합니다.
+     *
+     * @param seed
+     * @param length
+     */
     @Override
     public String generateWithSeed(String seed, int length) {
-        long seedLong = seed.hashCode();
-        Random randomGenerator = new Random(seedLong);
+        Random randomGenerator = new Random(seed.hashCode());
+        return generateHelper(length, randomGenerator);
+    }
+
+
+    private String generateHelper(int length, Random random) {
         StringBuilder randomStr = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            int number = randomGenerator.nextInt(DEFAULT_CHARSET.length());
+            int number = random.nextInt(DEFAULT_CHARSET.length());
             char ch = DEFAULT_CHARSET.charAt(number);
             randomStr.append(ch);
         }
         return randomStr.toString();
     }
-
 }
