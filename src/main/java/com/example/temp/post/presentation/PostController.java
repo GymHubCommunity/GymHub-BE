a195c378bcee8f8194352d06c0da72224bad16a4
JWbase <jungwoo1002@gmail.com>
2024-02-12 18:23:23 +0900

    [GymHubCommunity#54] feat: 게시글 작성 Controller 작성

--- src/main/java/com/example/temp/post/presentation/PostController.java
+++ src/main/java/com/example/temp/post/presentation/PostController.java
@@ -5,12 +5,17 @@ import static org.springframework.data.domain.Sort.Direction.DESC;
 import com.example.temp.common.annotation.Login;
 import com.example.temp.common.dto.UserContext;
 import com.example.temp.post.application.PostService;
+import com.example.temp.post.dto.request.PostCreateRequest;
 import com.example.temp.post.dto.response.PagePostResponse;
+import com.example.temp.post.dto.response.PostCreateResponse;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostCreateResponse> createPost(@Login UserContext userContext,
+        @RequestBody PostCreateRequest postCreateRequest) {
+        LocalDateTime registeredAt = LocalDateTime.now();
+        return ResponseEntity.ok(postService.create(userContext, postCreateRequest, registeredAt));
+    }
 
     @GetMapping
     public ResponseEntity<PagePostResponse> getFollowingPosts(@Login UserContext userContext,
