fccd9dc4bb41b2bff49a6b50e95df4b1ada17680
JWbase <jungwoo1002@gmail.com>
2024-02-12 18:22:23 +0900

    [GymHubCommunity#54] feat: 게시글 작성에 필요한 dto 추가

---
+++ src/main/java/com/example/temp/post/dto/request/PostCreateRequest.java
@@ -0,0 +1,23 @@
+package com.example.temp.post.dto.request;
+
+import com.example.temp.member.domain.Member;
+import com.example.temp.post.domain.Content;
+import com.example.temp.post.domain.Post;
+import com.example.temp.post.domain.PostImage;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record PostCreateRequest(
+    String content,
+    List<String> imageUrl
+) {
+
+    public Post toEntity(Member member, LocalDateTime registeredAt, List<PostImage> postImages) {
+        return Post.builder()
+            .member(member)
+            .content(Content.create(content))
+            .postImages(postImages)
+            .registeredAt(registeredAt)
+            .build();
+    }
+}
