b0a81b68adc3b3818d3c4508c366e57699f68783
JWbase <jungwoo1002@gmail.com>
2024-02-14 00:40:31 +0900

    [GymHubCommunity#54] refactor: PostCreateRequest imageUrl 필드에 @Nullable 명시

--- src/main/java/com/example/temp/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/example/temp/post/dto/request/PostCreateRequest.java
@@ -3,11 +3,13 @@ package com.example.temp.post.dto.request;
 import com.example.temp.member.domain.Member;
 import com.example.temp.post.domain.Content;
 import com.example.temp.post.domain.Post;
+import jakarta.annotation.Nullable;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record PostCreateRequest(
     String content,
+    @Nullable
     List<String> imageUrl
 ) {
 
