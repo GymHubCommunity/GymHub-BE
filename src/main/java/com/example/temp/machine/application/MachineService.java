cd163c19c973d65712a7b3770b0568d10c3e20f3
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-14 15:40:42 +0900

    [#76] feat: Machine 엔티티, 레포지토리 생성

--- src/main/java/com/example/temp/machine/application/MachineService.java
+++ src/main/java/com/example/temp/machine/application/MachineService.java
@@ -1,6 +1,7 @@
 package com.example.temp.machine.application;
 
 import com.example.temp.admin.dto.request.MachineCreateRequest;
+import com.example.temp.machine.domain.MachineRepository;
 import com.example.temp.machine.dto.response.MachineCreateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MachineService {
+
+    private final MachineRepository machineRepository;
 
     public MachineCreateResponse create(MachineCreateRequest request) {
         return null;
