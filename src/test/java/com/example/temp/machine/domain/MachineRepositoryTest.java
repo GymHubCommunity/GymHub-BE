416cf3c6429b8dc7f327724a4c1d9bd4f84ef088
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-15 00:15:41 +0900

    [#76] test: findAllByBodyPart 비어있을 때 테스트 작성

--- src/test/java/com/example/temp/machine/domain/MachineRepositoryTest.java
+++ src/test/java/com/example/temp/machine/domain/MachineRepositoryTest.java
@@ -40,6 +40,23 @@ class MachineRepositoryTest {
             .contains(target1.getName(), target2.getName());
     }
 
+    @Test
+    @DisplayName("신체부위에 해당되는 운동기구가 없으면 비어있는 리스트를 가져온다.")
+    void findAllThatResultIsEmpty() throws Exception {
+
+        // given
+        saveMachine("머신1", BodyPart.BACK);
+        saveMachine("머신2", BodyPart.CARDIO);
+        saveMachine("머신3", BodyPart.BACK);
+        em.flush();
+        em.clear();
+
+        // when
+        List<Machine> results = machineRepository.findAllByBodyPart(BodyPart.SHOULDER);
+
+        // then
+        assertThat(results).isEmpty();
+    }
     private Machine saveMachine(String name, BodyPart bodyPart) {
         MachineBodyPart machineBodyPart = MachineBodyPart.create(bodyPart);
         Machine machine = Machine.builder()
