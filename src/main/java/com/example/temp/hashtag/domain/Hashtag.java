eabf46e67ebdd95bdb05f95975db4323df50a113
JWbase <jungwoo1002@gmail.com>
2024-02-15 19:38:24 +0900

    [GymHubCommunity#78] refactor: Hashtag 정규표현식 javadoc 추가

--- src/main/java/com/example/temp/hashtag/domain/Hashtag.java
+++ src/main/java/com/example/temp/hashtag/domain/Hashtag.java
@@ -22,6 +22,14 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class Hashtag extends BaseTimeEntity {
 
+    /**
+     * ^ : 문자열의 시작을 나타냅니다.
+     * # : 해시태그 기호를 나타냅니다.
+     * [\w가-힣]은 모든 영문자, 숫자, 밑줄, 그리고 한글을 포함하는 문자를 의미합니다.
+     * + : 앞의 문자나 그룹이 하나 이상 반복되는 패턴을 나타냅니다.
+     * $ : 문자열의 끝을 나타냅니다.
+     * #으로 시작해서 한글, 영문자, 숫자, 밑줄만 작성되었는지 확인하는 정규표현식 입니다.
+     */
     private static final String HASHTAG_REGEX = "^#[\\w가-힣]+$";
 
     @Id
