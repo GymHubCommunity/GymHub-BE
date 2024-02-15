4ab9fbc8da07b9e5094dc9a5bb1c518e342883e8
JWbase <jungwoo1002@gmail.com>
2024-02-15 13:11:02 +0900

    [GymHubCommunity#78] feat: Hashtag 저장 비즈니스 로직 추가

---
+++ src/main/java/com/example/temp/hashtag/application/HashtagService.java
@@ -0,0 +1,32 @@
+package com.example.temp.hashtag.application;
+
+import com.example.temp.hashtag.domain.Hashtag;
+import com.example.temp.hashtag.domain.HashtagRepository;
+import com.example.temp.post.domain.Post;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class HashtagService {
+
+    private final HashtagRepository hashtagRepository;
+
+    @Transactional
+    public List<Hashtag> saveHashtag(List<String> names, Post post) {
+        return names.stream()
+            .map(this::saveOrFind)
+            .toList();
+    }
+
+    private Hashtag saveOrFind(String name) {
+        return hashtagRepository
+            .findByName(name)
+            .orElseGet(() -> hashtagRepository.save(Hashtag.builder()
+                .name(name)
+                .build()));
+    }
+}
