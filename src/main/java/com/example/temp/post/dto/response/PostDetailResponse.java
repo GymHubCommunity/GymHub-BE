8869c33f76125da59ea12c5846940796ffb00ce5
JWbase <jungwoo1002@gmail.com>
2024-02-16 22:23:54 +0900

    [GymHubCommunity#84] feat: PostDetailResponse 추가

---
+++ src/main/java/com/example/temp/post/dto/response/PostDetailResponse.java
@@ -0,0 +1,43 @@
+package com.example.temp.post.dto.response;
+
+import com.example.temp.post.domain.Post;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record PostDetailResponse(
+    Long postId,
+    WriterInfo writerInfo,
+    String content,
+    List<String> imageUrl,
+    List<String> hashtags,
+    LocalDateTime registeredAt
+) {
+
+    public static PostDetailResponse from(Post post) {
+        return PostDetailResponse.builder()
+            .postId(post.getId())
+            .writerInfo(WriterInfo.from(post.getMember()))
+            .content(post.getContent())
+            .imageUrl(getImageUrls(post))
+            .hashtags(getHashtags(post))
+            .registeredAt(post.getRegisteredAt())
+            .build();
+    }
+
+    private static List<String> getImageUrls(Post post) {
+        return post.getPostImages().isEmpty() ? Collections.emptyList() :
+            post.getPostImages().stream()
+                .map(postImage -> postImage.getImage().getUrl())
+                .toList();
+    }
+
+    private static List<String> getHashtags(Post post) {
+        return post.getPostHashtags().isEmpty() ? Collections.emptyList() :
+            post.getPostHashtags().stream()
+                .map(hashtag -> hashtag.getHashtag().getName())
+                .toList();
+    }
+}
