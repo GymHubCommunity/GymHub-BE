2ea3dcdd0eac0f91a0cda531d86ab2e8d2cfe394
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-17 20:03:49 +0900

    [#90] chore: Security 기본 설정 비활성화

--- src/main/java/com/example/temp/common/config/SecurityConfig.java
+++ src/main/java/com/example/temp/common/config/SecurityConfig.java
@@ -2,10 +2,21 @@ package com.example.temp.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
+            .build();
+    }
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
