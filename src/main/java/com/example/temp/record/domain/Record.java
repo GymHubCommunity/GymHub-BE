f998c5c224ad0dae2fa335b12bde88f4e2ee5c7d
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-19 13:02:53 +0900

    [#111] feat: Record 엔티티 생성

---
+++ src/main/java/com/example/temp/record/domain/Record.java
@@ -0,0 +1,40 @@
+package com.example.temp.record.domain;
+
+import com.example.temp.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "records")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id")
+    private Long id;
+
+    @Column(name = "writer_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member writer;
+
+    private LocalDate recordDate;
+
+    @Builder
+    private Record(Member writer, LocalDate recordDate) {
+        this.writer = writer;
+        this.recordDate = recordDate;
+    }
+}
