68fa319fe1e2aac72c8f42e0f07d0f50dc62de31
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-19 13:49:58 +0900

    [#111] feat: RecordCreateRequest 생성

--- src/main/java/com/example/temp/record/dto/request/RecordCreateRequest.java
+++ src/main/java/com/example/temp/record/dto/request/RecordCreateRequest.java
@@ -1,5 +1,22 @@
 package com.example.temp.record.dto.request;
 
-public record RecordCreateRequest() {
+import java.util.List;
 
+public record RecordCreateRequest(
+    List<RecordsTrackCreateRequest> tracks
+) {
+
+    public record RecordsTrackCreateRequest(
+        String machineName,
+        List<TracksSetCreateRequest> sets
+    ) {
+
+        public record TracksSetCreateRequest(
+            int setNum,
+            int weight,
+            int repeatCnt
+        ) {
+
+        }
+    }
 }
