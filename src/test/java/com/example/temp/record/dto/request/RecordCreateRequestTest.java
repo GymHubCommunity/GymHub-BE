7e4876ceb86f8e97b8432217299368e79ca72ea9
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-19 22:17:58 +0900

    [#111] test: TrackCreateRequest의 toEntity 메서드가 Track을 잘 생성하는지 테스트 작성

--- src/test/java/com/example/temp/record/dto/request/RecordCreateRequestTest.java
+++ src/test/java/com/example/temp/record/dto/request/RecordCreateRequestTest.java
@@ -1,9 +1,13 @@
 package com.example.temp.record.dto.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 import com.example.temp.record.domain.SetInTrack;
+import com.example.temp.record.domain.Track;
+import com.example.temp.record.dto.request.RecordCreateRequest.TrackCreateRequest;
 import com.example.temp.record.dto.request.RecordCreateRequest.TrackCreateRequest.SetInTrackCreateRequest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +28,24 @@ class RecordCreateRequestTest {
         assertThat(setInTrack.getOrder()).isEqualTo(1);
         assertThat(setInTrack.getWeight()).isEqualTo(weight);
         assertThat(setInTrack.getRepeatCnt()).isEqualTo(repeatCnt);
+    }
+
+    @Test
+    @DisplayName("TrackCreateRequest의 toEntity 메서드로 Track을 생성한다.")
+    void createTrack() throws Exception {
+        // given
+        SetInTrackCreateRequest setInTrackCreateRequest = new SetInTrackCreateRequest(10, 1);
+        TrackCreateRequest request = new TrackCreateRequest("머신이름", List.of(setInTrackCreateRequest));
+
+        // when
+        Track track = request.toEntity();
+
+        // then
+        assertThat(track.getMachineName()).isEqualTo(request.machineName());
+        assertThat(track.getSetsInTrack()).hasSize(1)
+            .extracting("order", "weight", "repeatCnt")
+            .contains(
+                tuple(1, setInTrackCreateRequest.weight(), setInTrackCreateRequest.repeatCnt()));
     }
 
 }
