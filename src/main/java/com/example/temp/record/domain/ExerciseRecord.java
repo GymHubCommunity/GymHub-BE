8619e5e3513bd9597259d2cd4e7b2635d7522158
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-19 15:21:49 +0900

    [#111] feat: Record -> ExerciseRecord로 이름 변경

--- src/main/java/com/example/temp/record/domain/ExerciseRecord.java
+++ src/main/java/com/example/temp/record/domain/ExerciseRecord.java
@@ -2,6 +2,7 @@ package com.example.temp.record.domain;
 
 import com.example.temp.common.entity.BaseTimeEntity;
 import com.example.temp.member.domain.Member;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,18 +11,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Table(name = "records")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Record extends BaseTimeEntity {
+public class ExerciseRecord extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,11 +37,27 @@ public class Record extends BaseTimeEntity {
     @JoinColumn(name = "writer_id", nullable = false)
     private Member writer;
 
+    @OneToMany(mappedBy = "exerciseRecord", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Track> tracks = new ArrayList<>();
+
+    @CreatedDate
     private LocalDate recordDate;
 
     @Builder
-    private Record(Member writer, LocalDate recordDate) {
+    private ExerciseRecord(Member writer, List<Track> tracks, LocalDate recordDate) {
         this.writer = writer;
         this.recordDate = recordDate;
+        if (tracks != null) {
+            this.tracks = new ArrayList<>();
+            this.tracks.addAll(tracks);
+        }
+    }
+
+    public static ExerciseRecord create(Member writer, List<Track> tracks) {
+        ExerciseRecord exerciseRecord = ExerciseRecord.builder()
+            .writer(writer)
+            .build();
+        tracks.forEach(track -> track.relate(exerciseRecord));
+        return exerciseRecord;
     }
 }
--- src/main/java/com/example/temp/record/domain/RecordRepository.java
+++ src/main/java/com/example/temp/record/domain/RecordRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecordRepository extends JpaRepository<Record, Long> {
+public interface RecordRepository extends JpaRepository<ExerciseRecord, Long> {
 
 }
