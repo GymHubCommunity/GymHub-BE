8b9b07bbe3a873e21d4b24c646c6e19d2da24666
JWbase <jungwoo1002@gmail.com>
2024-02-19 23:04:15 +0900

    [GymHubCommunity#115] feat: findAllByMemberId 메서드 추가

--- src/main/java/com/example/temp/post/domain/PostRepository.java
+++ src/main/java/com/example/temp/post/domain/PostRepository.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Slice<Post> findByMemberInOrderByRegisteredAtDesc(List<Member> members, Pageable pageable);
+
+    List<Post> findAllByMemberId(@Param("memberId") Long memberId);
 }
