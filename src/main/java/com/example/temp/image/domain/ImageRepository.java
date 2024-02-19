ede022c99c684549ea18e7b32776ade1ab687614
JWbase <jungwoo1002@gmail.com>
2024-02-19 20:16:26 +0900

    [GymHubCommunity#110] feat: findByUrlIn 메서드 추가

--- src/main/java/com/example/temp/image/domain/ImageRepository.java
+++ src/main/java/com/example/temp/image/domain/ImageRepository.java
@@ -1,5 +1,6 @@
 package com.example.temp.image.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,7 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
     boolean existsByUrl(String url);
 
     Image findByUrl(String url);
+
+    List<Image> findByUrlIn(List<String> urls);
 
 }
