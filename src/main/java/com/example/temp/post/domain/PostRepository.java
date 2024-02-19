7a57ec236ba58633bca684104b0477ef8bfc0c9c
JWbase <jungwoo1002@gmail.com>
2024-02-19 13:59:26 +0900

    [GymHubCommunity#112] refactor: findByMemberInOrderByRegisteredAtDesc 메서드가 Page에서 Slice를 반환하도록 수정

--- src/main/java/com/example/temp/post/domain/PostRepository.java
+++ src/main/java/com/example/temp/post/domain/PostRepository.java
@@ -2,13 +2,13 @@ package com.example.temp.post.domain;
 
 import com.example.temp.member.domain.Member;
 import java.util.List;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Page<Post> findByMemberInOrderByRegisteredAtDesc(List<Member> members, Pageable pageable);
+    Slice<Post> findByMemberInOrderByRegisteredAtDesc(List<Member> members, Pageable pageable);
 }
