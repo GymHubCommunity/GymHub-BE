6d6fbbbb98be39a9462a6bd219121c124bb35325
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-19 23:00:12 +0900

    [#x] feat: ExerciseRecord 생성시 응답으로 CreatedResponse 반환하도록 구현

--- src/main/java/com/example/temp/record/presentation/ExerciseRecordController.java
+++ src/main/java/com/example/temp/record/presentation/ExerciseRecordController.java
@@ -1,10 +1,12 @@
 package com.example.temp.record.presentation;
 
 import com.example.temp.common.annotation.Login;
+import com.example.temp.common.dto.CreatedResponse;
 import com.example.temp.common.dto.UserContext;
 import com.example.temp.record.application.ExerciseRecordService;
 import com.example.temp.record.dto.request.ExerciseRecordCreateRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,9 @@ public class ExerciseRecordController {
     private final ExerciseRecordService exerciseRecordService;
 
     @PostMapping
-    public ResponseEntity<Void> create(@Login UserContext userContext, ExerciseRecordCreateRequest request) {
+    public ResponseEntity<CreatedResponse> create(@Login UserContext userContext, ExerciseRecordCreateRequest request) {
         long createdId = exerciseRecordService.create(userContext, request);
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(CreatedResponse.of(createdId));
     }
 }
