6c03c2bbc298440a524294d2d1a38afaf4204cec
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-20 12:23:59 +0900

    [#119] feat: 신체부위 추가

--- src/main/java/com/example/temp/machine/domain/BodyPart.java
+++ src/main/java/com/example/temp/machine/domain/BodyPart.java
@@ -5,9 +5,15 @@ import lombok.Getter;
 @Getter
 public enum BodyPart {
 
+    CHEST("가슴"),
     BACK("등"),
     SHOULDER("어깨"),
+    TRICEPS("삼두"),
+    BICEPS("이두"),
+    FOREARM("전완근"),
     LEG("다리"),
+    HIP("엉덩이"),
+    CORE("코어"),
     CARDIO("유산소");
 
     private final String text;
