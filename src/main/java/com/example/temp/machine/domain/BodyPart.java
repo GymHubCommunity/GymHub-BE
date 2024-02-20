6f1f9f9940c95a746c91891e36bc4e5c29b26bc4
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-20 12:29:02 +0900

    [#119] feat: 신체부위 카테고리 추가

--- src/main/java/com/example/temp/machine/domain/BodyPart.java
+++ src/main/java/com/example/temp/machine/domain/BodyPart.java
@@ -1,24 +1,44 @@
 package com.example.temp.machine.domain;
 
+import static com.example.temp.machine.domain.BodyPart.BodyCategory.LOWER;
+import static com.example.temp.machine.domain.BodyPart.BodyCategory.UPPER;
+import static com.example.temp.machine.domain.BodyPart.BodyCategory.WHOLE;
+
 import lombok.Getter;
 
 @Getter
 public enum BodyPart {
 
-    CHEST("가슴"),
-    BACK("등"),
-    SHOULDER("어깨"),
-    TRICEPS("삼두"),
-    BICEPS("이두"),
-    FOREARM("전완근"),
-    LEG("다리"),
-    HIP("엉덩이"),
-    CORE("코어"),
-    CARDIO("유산소");
+    CHEST("가슴", UPPER),
+    BACK("등", UPPER),
+    SHOULDER("어깨", UPPER),
+    TRICEPS("삼두", UPPER),
+    BICEPS("이두", UPPER),
+    FOREARM("전완근", UPPER),
+    LEG("다리", LOWER),
+    HIP("엉덩이", LOWER),
+    CORE("코어", WHOLE),
+    CARDIO("유산소", WHOLE);
 
     private final String text;
+    private final BodyCategory category;
 
-    BodyPart(String text) {
+    BodyPart(String text, BodyCategory category) {
         this.text = text;
+        this.category = category;
+    }
+
+    @Getter
+    public enum BodyCategory {
+
+        UPPER("상체"),
+        LOWER("하체"),
+        WHOLE("전신");
+
+        private final String text;
+
+        BodyCategory(String text) {
+            this.text = text;
+        }
     }
 }
