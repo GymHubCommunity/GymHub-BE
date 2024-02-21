9cd142a556573c5dfa8963b1b1618f87ee968d20
JWbase <jungwoo1002@gmail.com>
2024-02-21 22:35:20 +0900

    [GymHubCommunity#121] feat: Comment 엔티티 추가

---
+++ src/main/java/com/example/temp/comment/domain/Comment.java
@@ -0,0 +1,40 @@
+package com.example.temp.comment.domain;
+
+import com.example.temp.member.domain.Member;
+import com.example.temp.post.domain.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Embedded
+    private Message message;
+}
