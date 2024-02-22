16ee64f1ef0c5e37ad9b5644164edd7c0624c4a9
JWbase <jungwoo1002@gmail.com>
2024-02-22 14:47:02 +0900

    [GymHubCommunity#121] refactor: CreatedResponse DTO를 사용하도록 수정

--- src/main/java/com/example/temp/comment/dto/response/CommentCreateResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.temp.comment.dto.response;
-
-public record CommentCreateResponse(Long commentId) {
-
-    public static CommentCreateResponse create(Long commentId) {
-        return new CommentCreateResponse(commentId);
-    }
-}
--- src/main/java/com/example/temp/comment/presentation/CommentController.java
+++ src/main/java/com/example/temp/comment/presentation/CommentController.java
@@ -2,8 +2,8 @@ package com.example.temp.comment.presentation;
 
 import com.example.temp.comment.application.CommentService;
 import com.example.temp.comment.dto.request.CommentCreateRequest;
-import com.example.temp.comment.dto.response.CommentCreateResponse;
 import com.example.temp.common.annotation.Login;
+import com.example.temp.common.dto.CreatedResponse;
 import com.example.temp.common.dto.UserContext;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +20,11 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentCreateResponse> createComment(@PathVariable Long postId,
+    public ResponseEntity<CreatedResponse> createComment(@PathVariable Long postId,
         @Login UserContext userContext,
         @RequestBody CommentCreateRequest commentCreateRequest) {
         LocalDateTime registeredAt = LocalDateTime.now();
         Long commentId = commentService.createComment(postId, userContext, commentCreateRequest, registeredAt);
-        return ResponseEntity.ok(CommentCreateResponse.create(commentId));
+        return ResponseEntity.ok(CreatedResponse.of(commentId));
     }
 }
