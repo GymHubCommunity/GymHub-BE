2b3b10f16c159aa3f851a165b4a532b0e7e168a7
JWbase <jungwoo1002@gmail.com>
2024-02-22 20:58:03 +0900

    [GymHubCommunity#138] test: CommentRepository Member 부분 JOIN으로 변경

--- src/main/java/com/example/temp/comment/domain/CommentRepository.java
+++ src/main/java/com/example/temp/comment/domain/CommentRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN FETCH c.member JOIN FETCH c.post WHERE c.post.id = :postId ORDER BY c.registeredAt DESC")
+    @Query("SELECT c FROM Comment c JOIN c.member JOIN FETCH c.post WHERE c.post.id = :postId ORDER BY c.registeredAt DESC")
     Slice<Comment> findByPostId(@Param("postId") Long postId, Pageable pageable);
 }
