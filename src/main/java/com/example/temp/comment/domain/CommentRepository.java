465237687fab3b3551e1da19fccdc0613e43f592
JWbase <jungwoo1002@gmail.com>
2024-02-22 18:52:19 +0900

    [GymHubCommunity#138] feat: CommentRepository 구현 및 findByPostId 메서드 추가

--- src/main/java/com/example/temp/comment/domain/CommentRepository.java
+++ src/main/java/com/example/temp/comment/domain/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.example.temp.comment.domain;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @Query("SELECT c FROM Comment c JOIN FETCH c.member JOIN FETCH c.post WHERE c.post.id = :postId ORDER BY c.registeredAt DESC")
+    Slice<Comment> findByPostId(@Param("postId") Long postId, Pageable pageable);
 }
