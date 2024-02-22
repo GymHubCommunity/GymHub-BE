8aa19ebe78f1cf30ac6e82cc7a36d8859f5bb480
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-22 23:02:01 +0900

    [#136] refactor: DTO에 존재하는 비즈니스 로직을 Service 계층으로 이동

--- src/main/java/com/example/temp/record/application/ExerciseRecordService.java
+++ src/main/java/com/example/temp/record/application/ExerciseRecordService.java
@@ -12,7 +12,10 @@ import com.example.temp.record.dto.request.ExerciseRecordCreateRequest;
 import com.example.temp.record.dto.request.ExerciseRecordUpdateRequest;
 import com.example.temp.record.dto.response.RetrievePeriodExerciseRecordsResponse;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,12 +39,32 @@ public class ExerciseRecordService {
     public RetrievePeriodExerciseRecordsResponse retrievePeriodExerciseRecords(UserContext userContext,
         DatePeriod datePeriod) {
         Member member = findMember(userContext);
-        LocalDate firstDate = datePeriod.getStartDate();
 
         List<ExerciseRecord> exerciseRecords = exerciseRecordRepository.findAllByMemberAndPeriod(member,
             datePeriod.getStartDate(), datePeriod.getLastDate());
-        return RetrievePeriodExerciseRecordsResponse.of(firstDate.getYear(), firstDate.getMonthValue(),
-            exerciseRecords);
+        Map<LocalDate, List<ExerciseRecord>> exerciseRecordMap = convertListToMapByDate(datePeriod, exerciseRecords);
+        return RetrievePeriodExerciseRecordsResponse.from(exerciseRecordMap);
+    }
+
+    private Map<LocalDate, List<ExerciseRecord>> convertListToMapByDate(DatePeriod datePeriod, List<ExerciseRecord> exerciseRecords) {
+        Map<LocalDate, List<ExerciseRecord>> result = createExerciseRecordMapByDate(datePeriod);
+        for (ExerciseRecord exerciseRecord : exerciseRecords) {
+            if (!result.containsKey(exerciseRecord.getRecordDate())) {
+                throw new IllegalArgumentException("year와 Month가 일치하지 않는 운동 결과가 발생했습니다.");
+            }
+            result.get(exerciseRecord.getRecordDate()).add(exerciseRecord);
+        }
+        return result;
+    }
+
+    private Map<LocalDate, List<ExerciseRecord>> createExerciseRecordMapByDate(DatePeriod datePeriod) {
+        Map<LocalDate, List<ExerciseRecord>> result = new HashMap<>();
+        LocalDate cursor = datePeriod.getStartDate();
+        while (!cursor.isAfter(datePeriod.getLastDate())) {
+            result.put(cursor, new ArrayList<>());
+            cursor = cursor.plusDays(1L);
+        }
+        return result;
     }
 
     /**
