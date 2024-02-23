3d8768405911af2edf78719a5a2bf5ec81bfe518
JWbase <jungwoo1002@gmail.com>
2024-02-23 18:53:59 +0900

    [GymHubCommunity#158] refactor: BaseTimeEntity 상속

--- src/main/java/com/example/temp/image/domain/Image.java
+++ src/main/java/com/example/temp/image/domain/Image.java
@@ -1,5 +1,6 @@
 package com.example.temp.image.domain;
 
+import com.example.temp.common.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "images")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Image {
+public class Image extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
