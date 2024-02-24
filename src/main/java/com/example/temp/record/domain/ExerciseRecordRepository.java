8f78d749aee24b52e621726c6f329e43a6700554
kimtaehoonDev <thkimm.dev@gmail.com>
2024-02-24 17:37:44 +0900

    [#135] feat: ExerciseRecordRepository 기본 메서드들 isSnapshot=false 조건이 적용되도록 JPQL로 변경

--- src/main/java/com/example/temp/record/domain/ExerciseRecordRepository.java
+++ src/main/java/com/example/temp/record/domain/ExerciseRecordRepository.java
@@ -3,6 +3,7 @@ package com.example.temp.record.domain;
 import com.example.temp.member.domain.Member;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,11 +12,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExerciseRecordRepository extends JpaRepository<ExerciseRecord, Long> {
 
-    @Query("SELECT er FROM ExerciseRecord er"
-        + " WHERE er.member = :member"
-        + " AND er.isSnapshot = false"
-        + " AND :startDate <= er.recordDate"
-        + " AND er.recordDate <= :lastDate")
+    @Query("SELECT er FROM ExerciseRecord er "
+        + "WHERE er.isSnapshot = false "
+        + "AND er.id = :targetId")
+    Optional<ExerciseRecord> findById(@Param("targetId") long targetId);
+
+    @Query("SELECT er FROM ExerciseRecord er "
+        + "WHERE er.isSnapshot = false "
+        + "AND er.member = :member "
+        + "AND :startDate <= er.recordDate "
+        + "AND er.recordDate <= :lastDate")
     List<ExerciseRecord> findAllByMemberAndPeriod(@Param("member") Member member,
         @Param("startDate") LocalDate startDate, @Param("lastDate") LocalDate lastDate);
+
 }
