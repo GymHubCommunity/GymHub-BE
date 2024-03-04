d6246aa0ef71d0e0c2298990e562be25afb905ae
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-05 01:58:57 +0900

    [#176] feat: 디스코드로 전달할 에러 메세지 직렬화

--- src/main/java/com/example/temp/common/config/WebhookConfig.java
+++ src/main/java/com/example/temp/common/config/WebhookConfig.java
@@ -2,17 +2,22 @@ package com.example.temp.common.config;
 
 import com.example.temp.common.infrastructure.exceptionsender.DiscordExceptionSender;
 import com.example.temp.common.infrastructure.exceptionsender.ExceptionSender;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("!test")
+@RequiredArgsConstructor
 public class WebhookConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     ExceptionSender discordExceptionSender() {
-        return new DiscordExceptionSender();
+        return new DiscordExceptionSender(objectMapper);
     }
 
 }
--- src/main/java/com/example/temp/common/infrastructure/exceptionsender/DiscordExceptionSender.java
+++ src/main/java/com/example/temp/common/infrastructure/exceptionsender/DiscordExceptionSender.java
@@ -1,27 +1,77 @@
 package com.example.temp.common.infrastructure.exceptionsender;
 
+import com.example.temp.common.dto.UserContext;
 import com.example.temp.common.exception.ExceptionInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@RequiredArgsConstructor
 public class DiscordExceptionSender implements ExceptionSender {
 
     @Value("${webhookUrl}")
-    String webhookUrl;
+    private String webhookUrl;
+
+    private final ObjectMapper objectMapper;
 
     @Override
-    public void send(ExceptionInfo exceptionInfo) {
-        String message = "예외 발생: " + exceptionInfo.getMessage();
+    public void send(ExceptionInfo exceptionInfo) throws JsonProcessingException {
+        String body = objectMapper.writeValueAsString(new BodyValue(exceptionInfo));
         WebClient client = WebClient.builder()
             .baseUrl(webhookUrl)
             .build();
         client.post()
             .contentType(MediaType.APPLICATION_JSON)
-            .body(BodyInserters.fromValue("{\"content\":\"" + message + "\"}"))
+            .body(BodyInserters.fromValue(body))
             .retrieve()
             .bodyToMono(Void.class)
             .block();
+    }
+
+    @Getter
+    static class BodyValue implements Serializable {
+
+        private final List<Embed> embeds;
+
+        public BodyValue(ExceptionInfo exceptionInfo) {
+            embeds = List.of(new Embed(exceptionInfo));
+        }
+
+        @Getter
+        static class Embed implements Serializable {
+
+            private final String title;
+            private final String description;
+
+            public Embed(ExceptionInfo exceptionInfo) {
+                this.title = exceptionInfo.getClazz();
+                this.description = getDescription(exceptionInfo);
+            }
+
+            private String getDescription(ExceptionInfo exceptionInfo) {
+                Optional<UserContext> userContextOpt = exceptionInfo.getUserContextOpt();
+                return String.format(
+                    """
+                        **Endpoint**
+                        %s
+                        **Message**
+                        %s
+                        **Login User Info**
+                        %s
+                        """, exceptionInfo.getEndpoint(), exceptionInfo.getMessage(), getUserInfo(userContextOpt));
+            }
+
+            private String getUserInfo(Optional<UserContext> userContextOpt) {
+                return userContextOpt.isPresent() ? userContextOpt.get().toString() : "로그인하지 않은 사용자";
+            }
+        }
     }
 }
