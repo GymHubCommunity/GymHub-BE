202312d660fb7e014c3a1f166f046f6d41eb2148
JWbase <jungwoo1002@gmail.com>
2024-03-09 13:58:36 +0900

    [GymHubCommunity#180] feat: memberId로 게시글 조회 Controller 추가

---
+++ src/main/java/com/example/temp/post/presentation/UserPostController.java
@@ -0,0 +1,31 @@
+package com.example.temp.post.presentation;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+import com.example.temp.common.annotation.Login;
+import com.example.temp.common.dto.UserContext;
+import com.example.temp.post.application.PostService;
+import com.example.temp.post.dto.response.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserPostController {
+
+    private final PostService postService;
+
+    @GetMapping("/members/{memberId}/posts")
+    public ResponseEntity<PostResponse> getPostsByMember(
+        @PathVariable("memberId") Long memberId,
+        @Login UserContext userContext,
+        @PageableDefault(sort = "registeredAt", direction = DESC) Pageable pageable
+    ) {
+        return ResponseEntity.ok(postService.findPostsByMember(memberId, userContext, pageable));
+    }
+}
