6b6a3c5222da0e9a658033622bf0cda8f0c8ec50
JWbase <jungwoo1002@gmail.com>
2024-03-11 11:04:23 +0900

    [GymHubCommunity#180] refactor: UserPostController -> MemberPostController 클래스 명 변경

--- src/main/java/com/example/temp/post/presentation/MemberPostController.java
+++ src/main/java/com/example/temp/post/presentation/MemberPostController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class UserPostController {
+public class MemberPostController {
 
     private final PostService postService;
 
